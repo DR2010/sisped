88ed5d360bd226f3dcac5e96a391380bbee2f309
Daniel <daniellgmachado@gmail.com>
2020-11-06 08:39:13 +1100

    2nd commit

---
+++ repo.cmd
@@ -0,0 +1,7 @@
+echo "# sisped" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DR2010/sisped.git
+git push -u origin main
